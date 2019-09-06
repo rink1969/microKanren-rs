c82cb12346b2858d27a0358201d53b9ab2574df8
rink1969 <rink1969@cryptape.com>
2019-09-06 22:06:40 +0800

    work for very simple case

--- src/lib.rs
+++ src/lib.rs
@@ -90,7 +90,7 @@ impl Assoc {
 // Subst is a group of bindings
 #[derive(Debug, Clone)]
 pub struct Subst {
-    s: Vec<Assoc>
+    s: Vec<Assoc>,
 }
 
 impl Subst {
@@ -112,11 +112,12 @@ impl Subst {
 
     pub fn walk(&self, v: &Value) -> Value {
         let mut ret = v.clone();
-        let mut found = false;
+        let mut found;
         loop {
             if !ret.is_variable() {
                 return ret;
             }
+            found = false;
             for a in &self.s {
                 let tmp_v = Value::new_var(a.get_var().clone());
                 if tmp_v == ret {
@@ -135,59 +136,108 @@ impl Subst {
         let a = Assoc::new(var, val);
         self.s.push(a);
     }
+
+    pub fn clear(&mut self) {
+        self.s.clear();
+    }
 }
 
-pub fn unify(v0: &Value, v1: &Value, s: Subst) -> Option<Subst> {
+pub fn unify(v0: &Value, v1: &Value, s: &mut Subst) -> bool {
     let u = s.walk(v0);
     let v = s.walk(v1);
+
     if u.is_variable() && v.is_variable() && u == v {
-        return Some(s);
+        return true;
     }
-    let mut s = s;
     if u.is_variable() {
         s.ext(u.to_var().unwrap(), v);
-        return Some(s);
+        return true;
     }
     if v.is_variable() {
         s.ext(v.to_var().unwrap(), u);
-        return Some(s);
+        return true;
     }
-
     if u == v {
-        return Some(s);
+        return true;
     }
-    return None;
+
+    s.clear();
+    return false;
 }
 
-pub fn unify_subst(s0: &Subst, s1: &Subst, s: Subst) -> Option<Subst> {
-    if s0.len() != s1.len() {
-        return None;
-    }
-    let mut s = s;
-    for i in 0..s0.len() {
-        let a0 = s0.get_a(i);
-        let a1 = s1.get_a(i);
-        // unify var in assoc
-        let v0 = Value::new_var(a0.get_var().clone());
-        let v1 = Value::new_var(a1.get_var().clone());
-        let tmp_s = unify(&v0, &v1, s);
-        if tmp_s.is_none() {
-            return None;
-        }
-        s = tmp_s.unwrap();
-        // unify value in assoc
-        let v0 = a0.get_val().clone();
-        let v1 = a1.get_val().clone();
-        let tmp_s = unify(&v0, &v1, s);
-        if tmp_s.is_none() {
-            return None;
-        }
-        s = tmp_s.unwrap();
-    }
-    Some(s)
+// State include a Subst and next variable index
+#[derive(Debug, Clone)]
+pub struct State {
+    s: Subst,
+    c: usize,
 }
 
+impl State {
+    pub fn new(s: Subst, c: usize) -> Self {
+        State { s, c }
+    }
 
+    pub fn empty_state() -> Self {
+        State {
+            s: Subst::new(),
+            c: 0,
+        }
+    }
+
+    pub fn get_s(&mut self) -> &mut Subst {
+        &mut self.s
+    }
+
+    pub fn get_c(&self) -> usize {
+        self.c
+    }
+
+    pub fn inc_c(&mut self) {
+        self.c += 1;
+    }
+
+    pub fn set_s(&mut self, s: Subst) {
+        self.s = s
+    }
+
+    pub fn lookup(&self, v: &Value) -> Value {
+        self.s.walk(v)
+    }
+}
+
+pub fn fresh(s: &mut State) -> Var {
+    let v = Var::new_x(s.get_c());
+    s.inc_c();
+    println!("fresh new var {:?}", v);
+    v
+}
+
+pub fn same(v0: Value, v1: Value, s: &mut State) -> bool {
+    unify(&v0, &v1, s.get_s())
+}
+
+pub fn conj(g0: &Fn(&mut State) -> bool, g1: &Fn(&mut State) -> bool, s: &mut State) -> Vec<State> {
+    let mut ret = Vec::new();
+    if g0(s) {
+        if g1(s) {
+            ret.push(s.clone());
+        }
+    }
+    ret
+}
+
+pub fn disj(g0: &Fn(&mut State) -> bool, g1: &Fn(&mut State) -> bool, s: &mut State) -> Vec<State> {
+    let mut ret = Vec::new();
+    let mut s1 = s.clone();
+    if g0(s) {
+        ret.push(s.clone());
+    }
+
+    if g1(&mut s1) {
+        ret.push(s1);
+    }
+    ret
+}
 
 #[cfg(test)]
 mod test {
@@ -225,52 +275,63 @@ mod test {
     #[test]
     fn unify_basic() {
         let v0 = Value::Num(1);
-        let s = Subst::new();
-        let ret = unify(&v0, &v0, s);
-        println!("{:?}", ret);
-        assert_eq!(ret.is_some(), true);
+        let mut s = Subst::new();
+        let ret = unify(&v0, &v0, &mut s);
+        println!("{:?}", s);
+        assert_eq!(ret, true);
 
         let x0 = Var::new_x(0);
         let v1 = Value::new_var(x0);
-        let s = Subst::new();
-        let ret = unify(&v1, &v1, s);
-        println!("{:?}", ret);
-        assert_eq!(ret.is_some(), true);
+        let mut s = Subst::new();
+        let ret = unify(&v1, &v1, &mut s);
+        println!("{:?}", s);
+        assert_eq!(ret, true);
 
-        let s = Subst::new();
-        let ret = unify(&v0, &v1, s);
-        println!("{:?}", ret);
-        assert_eq!(ret.is_some(), true);
+        let mut s = Subst::new();
+        let ret = unify(&v0, &v1, &mut s);
+        println!("{:?}", s);
+        assert_eq!(ret, true);
 
-        let s = Subst::new();
-        let ret = unify(&v1, &v0, s);
-        println!("{:?}", ret);
-        assert_eq!(ret.is_some(), true);
+        let mut s = Subst::new();
+        let ret = unify(&v1, &v0, &mut s);
+        println!("{:?}", s);
+        assert_eq!(ret, true);
 
-        let s = Subst::new();
+        let mut s = Subst::new();
         let v2 = Value::Num(2);
-        let ret = unify(&v0, &v2, s);
-        println!("{:?}", ret);
-        assert_eq!(ret.is_some(), false);
+        let ret = unify(&v0, &v2, &mut s);
+        println!("{:?}", s);
+        assert_eq!(ret, false);
     }
 
     #[test]
-    fn unify_subst_basic() {
-        // 1 = x0
-        let s = unify(&Value::new_num(1), &Value::new_var(Var::new_x(0)), Subst::new()).unwrap();
-        println!("s = {:?}", s);
-        // x0 = x1
-        let s1 = unify(&Value::new_var(Var::new_x(0)), &Value::new_var(Var::new_x(1)), Subst::new()).unwrap();
-        println!("s1 = {:?}", s1);
-        // 2 = x1
-        let s2 = unify(&Value::new_num(2), &Value::new_var(Var::new_x(1)), Subst::new()).unwrap();
-        println!("s2 = {:?}", s2);
-        assert_eq!(unify_subst(&s1, &s2, s.clone()).is_some(), false);
+    fn goal_basic() {
+        let mut s = State::empty_state();
+        let x0 = fresh(&mut s);
+        let ret = same(Value::new_var(x0.clone()), Value::new_num(1), &mut s);
+        assert_eq!(ret, true);
+        let x1 = fresh(&mut s);
+        let ret = same(Value::new_var(x0), Value::new_var(x1.clone()), &mut s);
+        assert_eq!(ret, true);
+        println!("x1 = {:?}", s.lookup(&Value::new_var(x1)));
+    }
 
-        // 1 = x1
-        let s3 = unify(&Value::new_num(1), &Value::new_var(Var::new_x(1)), Subst::new()).unwrap();
-        println!("s3 = {:?}", s3);
-        let s4 = unify_subst(&s1, &s3, s).unwrap();
-        println!("s4 = {:?}", s4);
+    fn fives(x: Var, s: &mut State) -> bool {
+        same(Value::new_var(x), Value::new_num(5), s)
+    }
+
+    fn six(x: Var, s: &mut State) -> bool {
+        same(Value::new_var(x), Value::new_num(6), s)
+    }
+
+    fn fivesorsix(x: Var, s: &mut State) -> Vec<State> {
+        disj(&|s1| fives(x.clone(), s1), &|s2| six(x.clone(), s2), s)
+    }
+
+    #[test]
+    fn goal_complex() {
+        let mut s = State::empty_state();
+        let x0 = fresh(&mut s);
+        println!("result = {:?}", fivesorsix(x0.clone(), &mut s));
     }
 }
